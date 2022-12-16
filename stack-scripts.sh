1a32d11164b24c1aa2c0732154548ed711f49e4b
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-16 16:14:05 +0530

    Create stack-scripts.sh

---
+++ stack-scripts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export STACK_NAME=my-bucket-stack
+export STACK_TEMPLATE_FILE=amazon-eks-vpc-private-subnets.yaml
+
+output=$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} 2>&1)
+
+if [ $? -ne 0 ]; then
+  if echo ${output} | grep -q ValidationError; then
+        echo 'creating stack....' ${STACK_NAME}
+		aws cloudformation deploy --template-file ${STACK_TEMPLATE_FILE} --stack-name ${STACK_NAME}
+  else
+    >&2 echo "Error is -->" ${output}
+  fi
+fi
+#>&2 means send the output to STDERR, So it will print the message as an error on the console.
